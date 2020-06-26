f4b3a1ec34601d2b9d898ab9998cf838852c54c1
Bob G <rjcgraham@gmail.com>
2020-06-26 09:54:56 -0700

    initial server and client

---
+++ data.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+    return {
+        categories: ["Drinks", "Sandwiches", "Treats"],
+        items : [ 
+            
+            {id: 1, name: "Cola", category: "Drinks", description: "A classic refersher", price: 1.50},
+            {id: 2, name: "Roast Beef", category: "Sandwiches", description: "Thin-sliced meat on fresh bread", price: 6.50},
+            {id: 3, name: "Cookie", category: "Treats", description: "Choice of Giant-Sized Chocalate Chip or Peanut Butter", price: 2.00},    
+    ],
+        orders: []
+    }
+}
